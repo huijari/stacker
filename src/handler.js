3e2af85cef1de442fd1219260565acd1429a19cd
Alexandre Cesar <acmorcego@gmail.com>
2018-04-18 19:47:25 -0300

    add setup command, still need to convert the image

---
+++ src/handler.js
@@ -0,0 +1,27 @@
+const { v4 } = require('uuid')
+
+const { createMeta } = require('./data')
+
+const setup = async ({ message, from, reply, telegram }) => {
+  if (!message.reply_to_message || !message.reply_to_message.sticker)
+    return void reply('Please reply to a sticker')
+
+  const { username } = await telegram.getMe()
+  const id = v4().replace(/-/g, '')
+  const name = `pack_${id}_by_${username}`
+  const title = `${message.chat.title}'s Top Stickers`
+
+  const result = await telegram.createNewStickerSet(from.id, name, title, {
+    png_sticker: message.reply_to_message.sticker.file_id,
+    emojis: message.reply_to_message.sticker.emoji,
+  })
+
+  if (result) {
+    await createMeta(message.chat.id, name)
+    reply('Success')
+  }
+}
+
+module.exports = {
+  setup
+}
