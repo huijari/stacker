3cab148ff24fd27ca84527e6ffa06cbc85aeed43
Alexandre Cesar <acmorcego@gmail.com>
2018-05-04 20:45:28 -0300

    use converted image in the setup

--- src/handler/setup.js
+++ src/handler/setup.js
@@ -1,6 +1,7 @@
 const { v4 } = require('uuid')
 
 const { createMeta } = require('../data')
+const { toPng } = require('../image')
 
 const setup = async ({ message, from, reply, telegram }) => {
   if (!message.reply_to_message || !message.reply_to_message.sticker)
@@ -11,14 +12,19 @@ const setup = async ({ message, from, reply, telegram }) => {
   const name = `pack_${id}_by_${username}`
   const title = `${message.chat.title}'s Top Stickers`
 
+  const stickerLink = await telegram.getFileLink(
+    message.reply_to_message.sticker.file_id
+  )
+  const source = await toPng(stickerLink)
+
   const result = await telegram.createNewStickerSet(from.id, name, title, {
-    png_sticker: message.reply_to_message.sticker.file_id,
+    png_sticker: { source },
     emojis: message.reply_to_message.sticker.emoji
   })
 
   if (result) {
     await createMeta(message.chat.id, name)
-    reply('Success')
+    reply(`Success! t.me/addstickers/${name}`)
   }
 }
 
