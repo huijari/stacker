d07763b93d5925ef5879ff9e920cd0c88427e601
Alexandre Cesar <acmorcego@gmail.com>
2018-05-04 22:04:21 -0300

    configure webhook

--- src/bot.js
+++ src/bot.js
@@ -8,7 +8,11 @@ function start() {
   bot.command('/setup', setup)
   bot.on('sticker', vote)
 
-  bot.startPolling()
+  if (process.env['HOOK_URL']) {
+    bot.telegram.setWebhook(`${process.env['HOOK_URL']}/stacker`)
+    bot.startWebhook('/stacker', null, process.env['PORT'])
+  } else
+    bot.startPolling()
 }
 
 module.exports = start
